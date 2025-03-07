cad526d9b11a30fbcff1c8582e149478fd840a18
ptsouth97 <ptsouth97@gmail.com>
2025-03-07 10:14:36 -0500

    removed main from gravity.py

--- gravity.py
+++ gravity.py
@@ -1,15 +1,11 @@
 import constants
-def main():
-	''' Enter masses and radius in scientific notation form'''
-	G=constants.G
-	m1=float(input("m1= "))
-	e1=float(input("e1= "))
-	m2=float(input("m2= "))
-	e2=float(input("e2= "))
-	r=float(input("r= "))
-	e3=float(input("e3= "))
-	F=G*(m1*10**e1)*(m2*10**e2)/(r*10**e3)**2
-	print("Force = "+str(F)+"N")
-
-if __name__ == '__main__':
-	main()
+''' Enter masses and radius in scientific notation form'''
+G=constants.G
+m1=float(input("m1= "))
+e1=float(input("e1= "))
+m2=float(input("m2= "))
+e2=float(input("e2= "))
+r=float(input("r= "))
+e3=float(input("e3= "))
+F=G*(m1*10**e1)*(m2*10**e2)/(r*10**e3)**2
+print("Force = "+str(F)+"N")
