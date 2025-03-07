4e09982ef2ae56bbf47f433ac3248483600d7ae1
ptsouth97 <ptsouth97@gmail.com>
2025-03-07 07:19:11 -0500

    updated gravity.py

--- gravity.py
+++ gravity.py
@@ -1,5 +1,9 @@
-m1=int(input("m1= "))
-m2=int(input("m2= "))
-r=int(input("r= "))
-F=m1*m2/r**2
+G=6.67*10**(0-11)
+m1=float(input("m1= "))
+e1=float(input("e1= "))
+m2=float(input("m2= "))
+e2=float(input("e2= "))
+r=float(input("r= "))
+e3=float(input("e3= "))
+F=G*(m1*10**e1)*(m2*10**e2)/(r*10**e3)**2
 print(f'Force = {F} N')
