b32331c534b480e9c01aac316b6da730f6d3db63
ptsouth97 <ptsouth97@gmail.com>
2025-03-07 07:53:52 -0500

    added gravity main function

--- gravity.py
+++ gravity.py
@@ -1,10 +1,14 @@
-# Enter masses and radius in scientific notation form
-G=6.67*10**(0-11)
-m1=float(input("m1= "))
-e1=float(input("e1= "))
-m2=float(input("m2= "))
-e2=float(input("e2= "))
-r=float(input("r= "))
-e3=float(input("e3= "))
-F=G*(m1*10**e1)*(m2*10**e2)/(r*10**e3)**2
-print(f'Force = {F} N')
+def main():
+	''' Enter masses and radius in scientific notation form'''
+	G=6.67*10**(0-11)
+	m1=float(input("m1= "))
+	e1=float(input("e1= "))
+	m2=float(input("m2= "))
+	e2=float(input("e2= "))
+	r=float(input("r= "))
+	e3=float(input("e3= "))
+	F=G*(m1*10**e1)*(m2*10**e2)/(r*10**e3)**2
+	print("Force = "+str(F)+"N")
+
+if __name__ == '__main__':
+	main()
